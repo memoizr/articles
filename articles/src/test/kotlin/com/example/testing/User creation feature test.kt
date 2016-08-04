bcce6a3c45d1da602f2c6ac0c676342dc6123345
memoizr <memoizrlabs@gmail.com>
2016-08-04 11:40:55 +0100

    add images

---
+++ articles/src/test/kotlin/com/example/testing/User creation feature test.kt
@@ -0,0 +1,33 @@
+package com.example.testing
+
+import org.junit.Test
+
+class `User creation feature test` {
+    @Test
+    fun `Displays success screen when creating a user with all fields valid`() {
+    }
+
+    @Test
+    fun `Does not send request when creating a user with any field invalid`() {
+    }
+
+    @Test
+    fun `Sends latest data when creating a user after view is reattached`() {
+    }
+
+    @Test
+    fun `Shows error when creating a user with no valid email`() {
+    }
+
+    @Test
+    fun `Shows error when creating a user with no valid username`() {
+    }
+
+    @Test
+    fun `Shows error when creating a user and requests fails`() {
+    }
+
+    @Test
+    fun `Sends request when creating a user with all fields valid`() {
+    }
+}
---
+++ articles/src/test/kotlin/com/example/testing/UserCreationFeatureTest.kt
@@ -0,0 +1,34 @@
+package com.example.testing
+
+import org.junit.Test
+
+class UserCreationFeatureTest {
+    @Test
+    fun givenAllFieldsAreValidWhenCreatingUserThenDisplaysSuccessScreen() {
+    }
+
+    @Test
+    fun givenAnyFieldIsInvalidWhenCreatingUserThenDoesNotSendRequest() {
+    }
+
+    @Test
+    fun givenViewHasBeenReattachedWhenCreatingUserSendsLatestData() {
+    }
+
+    @Test
+    fun givenNoValidEmailWhenCreatingUserThenShowsError() {
+    }
+
+    @Test
+    fun givenNoValidUsernameWhenCreatingUserThenShowsError() {
+    }
+
+    @Test
+    fun givenLoginRequestFailsWhenLoggingInThenShowsError() {
+    }
+
+    @Test
+    fun givenAllFieldsAreValidWhenCreatingUserThenSendsRequest() {
+    }
+}
+
